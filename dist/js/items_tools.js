3090ab1687ecb778e8895064c056b8efb5c2a023
deadloxt <caglar44mm@gmail.com>
2026-05-14 19:46:43 +0300

    i forgor to change limit lel

--- dist/js/items_tools.js
+++ dist/js/items_tools.js
@@ -632,7 +632,7 @@ function item_decoder(file, using_editor) {
         var version = read_buffer_number(arrayBuffer, 0, 2);
         var item_count = read_buffer_number(arrayBuffer, 2, 4);
 
-        if (version > 24) {
+        if (version > 26) {
             return Swal.mixin({
                 toast: true,
                 position: 'top-end',
